1b96e3de0e1cb5ebd1630a3a482c7a779d58408b
Maxim Grigorev <maxkraft06@gmail.com>
2025-10-19 14:52:56 +0300

    processing an empty array

--- homeworks/hw3_var3/src/array.cpp
+++ homeworks/hw3_var3/src/array.cpp
@@ -5,21 +5,33 @@
 
 void Array::Add(std::shared_ptr<Figure> fig) {
     data.push_back(std::move(fig));
+    std::cout << "Figure added. Total figures: " << data.size() << "\n";
 }
 
 void Array::Remove(size_t index) {
     if (index >= data.size()) 
         throw std::out_of_range("Invalid index");
     data.erase(data.begin() + index);
+    std::cout << "Figure removed. Total figures: " << data.size() << "\n";
 }
 
 void Array::PrintAll() const {
+    if (data.empty()) {
+        std::cout << "No figures to display.\n";
+        return;
+    }
+
     for (size_t i = 0; i < data.size(); ++i)
         std::cout << i << ": " << *data[i]
                   << " | Area = " << static_cast<double>(*data[i]) << "\n";
 }
 
 void Array::PrintCenters() const {
+    if (data.empty()) {
+        std::cout << "No figures to display their centers.\n";
+        return;
+    }
+
     for (size_t i = 0; i < data.size(); ++i) {
         auto c = data[i]->Center();
         std::cout << i << ": Center = (" << c.first << ", " << c.second << ")\n";
