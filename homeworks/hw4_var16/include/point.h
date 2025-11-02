38302bb2ec82ba55bd969c1adea6897bc1a98d6f
Maxim Grigorev <maxkraft06@gmail.com>
2025-11-02 19:45:32 +0300

    point.h is pretty good I guess

---
+++ homeworks/hw4_var16/include/point.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <type_traits>
+#include <concepts>
+
+template <typename T>
+concept Scalar = std::is_scalar_v<T>;
+
+template <Scalar T>
+class Point {
+public:
+    T x{}, y{};
+
+    Point() = default;
+    Point(T _x, T _y) : x(_x), y(_y) {}
+    Point(const Point& other) = default;
+    Point(Point&& other) noexcept = default;
+
+    Point& operator=(const Point& other) = default;
+    Point& operator=(Point&& other) noexcept = default;
+
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+        return os << "(" << p.x << ", " << p.y << ")";
+    }
+
+    friend std::istream& operator>>(std::istream& is, Point& p) {
+        return is >> p.x >> p.y;
+    }
+};
