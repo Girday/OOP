f358e6064e3a415214109d8458a1ed1e82924fc5
Maxim Grigorev <maxkraft06@gmail.com>
2025-11-17 16:13:40 +0300

    add FixedBlockResource.h

---
+++ homeworks/hw5_var8/include/FixedBlockResource.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory_resource>
+#include <vector>
+
+class FixedBlockResource : public std::pmr::memory_resource {
+    public:
+        FixedBlockResource(size_t size);
+        ~FixedBlockResource();
+    
+    protected:
+        void* do_allocate(size_t bytes, size_t alignment) override;
+        void do_deallocate(void* ptr, size_t bytes, size_t alignment) override;
+        bool do_is_equal(const memory_resource& other) const noexcept override;
+    
+    private:
+        struct BlockInfo {
+            void* ptr;
+            size_t size;
+            bool free;
+        };
+
+        char* buffer;
+        size_t buffer_size;
+        size_t offset;
+        std::vector<BlockInfo> blocks;
+};
