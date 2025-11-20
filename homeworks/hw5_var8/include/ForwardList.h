0c712dc57237fecd0dd3f22627a092788e4dd495
Maxim Grigorev <maxkraft06@gmail.com>
2025-11-20 21:33:51 +0300

    add pop_front and pop_back to ForwardList

--- homeworks/hw5_var8/include/ForwardList.h
+++ homeworks/hw5_var8/include/ForwardList.h
@@ -9,6 +9,8 @@
 template <typename T>
 class ForwardList {
     public:
+        using Alloc = std::pmr::polymorphic_allocator<Node<T>>;
+
         ForwardList(std::pmr::memory_resource* res) : alloc(res) {}
 
         ~ForwardList() {
@@ -16,13 +18,27 @@ class ForwardList {
             
             while (curr) {
                 Node<T>* tmp = curr->next;
-                using Alloc = std::pmr::polymorphic_allocator<Node<T>>;
-                std::allocator_traits<Alloc>::destroy(alloc, curr);
-                alloc.deallocate(curr, 1);
+                destroy_node(curr);
                 curr = tmp;
             }
             
             head = nullptr;
+        }
+
+        void push_front(const T& value) {
+            Node<T>* new_node = alloc.allocate(1);
+            alloc.construct(new_node, Node<T>{value, head});
+            head = new_node;
+        }
+
+        void pop_front() {
+            if (!head)
+                return;
+            
+            Node<T>* old = head;
+            head = head->next;
+
+            destroy_node(old);
         }
 
         void push_back(const T& value) {
@@ -41,10 +57,23 @@ class ForwardList {
             curr->next = new_node;
         }
 
-        void push_front(const T& value) {
-            Node<T>* new_node = alloc.allocate(1);
-            alloc.construct(new_node, Node<T>{value, head});
-            head = new_node;
+        void pop_back() {
+            if (!head)
+                return;
+
+            if (!head->next) {
+                pop_front();
+                return;
+            }
+
+            Node<T>* curr = head;
+            while (curr->next && curr->next->next)
+                curr = curr->next;
+            
+            Node<T>* old = curr->next;
+            curr->next = nullptr;
+
+            destroy_node(old);
         }
 
         Node<T>* get_head() const {
@@ -60,6 +89,11 @@ class ForwardList {
         }
 
     private:
+        void destroy_node(Node<T>* node) {
+            std::allocator_traits<Alloc>::destroy(alloc, node);
+            alloc.deallocate(node, 1);
+        }
+
         Node<T>* head = nullptr;
-        std::pmr::polymorphic_allocator<Node<T>> alloc;
+        Alloc alloc;
 };
