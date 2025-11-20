6aa942888984b0d8f430642663aebb7d1b0af58a
Maxim Grigorev <maxkraft06@gmail.com>
2025-11-20 20:36:12 +0300

    add ForwardList

---
+++ homeworks/hw5_var8/include/ForwardList.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <memory_resource>
+#include <iterator>
+#include <stdexcept>
+
+#include "ListIterator.h"
+
+template <typename T>
+class ForwardList {
+    public:
+        ForwardList(std::pmr::memory_resource* res) : alloc(res) {}
+
+        ~ForwardList() {
+            Node<T>* curr = head;
+            
+            while (curr) {
+                Node<T>* tmp = curr->next;
+                using Alloc = std::pmr::polymorphic_allocator<Node<T>>;
+                std::allocator_traits<Alloc>::destroy(alloc, curr);
+                alloc.deallocate(curr, 1);
+                curr = tmp;
+            }
+            
+            head = nullptr;
+        }
+
+        void push(const T& value) {
+            Node<T>* new_node = alloc.allocate(1);
+            alloc.construct(new_node, Node<T>{value, head});
+            head = new_node;
+        }
+
+        Node<T>* get_head() const {
+            return head;
+        }
+
+        ListIterator<T> begin() {
+            return ListIterator<T>(head);
+        }
+
+        ListIterator<T> end() {
+            return ListIterator<T>(nullptr);
+        }
+
+    private:
+        Node<T>* head = nullptr;
+        std::pmr::polymorphic_allocator<Node<T>> alloc;
+};
